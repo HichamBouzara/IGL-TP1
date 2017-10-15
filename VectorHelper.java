df152fb1dec6a2d6ba983eff22298a11e57e7e01
HichemBZR <fa_bouzara@esi.dz>
2017-10-15 21:10:22 +0100

    L'initialisation de classe VectorHelper
    Avec documentation Javadoc introductionnaire (prologue).

---
+++ VectorHelper.java
@@ -0,0 +1,16 @@
+/**
+ *<h1>VectorHelper<h1>
+ * Le programme <b>VectorHelper</b> offre divers opérations sur les vecteurs,
+ * (i.e le tri, la somme de deux vecteurs, l'inversement des éléments d'un vecteur, le min et max
+ * et la multiplication des éléments par 2).
+ * <p>
+ * @author Aimene Hichem BOUZARA
+ * @author fa_bouzara@esi.dz
+ * @author Imene MEKIDECHE
+ * @author fi_mekideche@esi.dz
+ * @version 1.0
+ * @since 15-10-2017
+ */
+public class VectorHelper {
+    
+}
