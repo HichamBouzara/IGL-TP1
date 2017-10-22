83097fb9f62ea4181eddd32075c7c088582ce354
HichemBZR <fa_bouzara@esi.dz>
2017-10-22 15:18:58 +0100

    Hi ! I'm a commit

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,8 +1,8 @@
 /**
- *<h1>VectorHelper<h1>
- * Le programme <b>VectorHelper</b> offre divers opérations sur les vecteurs,
- * (i.e le tri, la somme de deux vecteurs, l'inversement des éléments d'un vecteur, le min et max
- * et la multiplication des éléments par 2).
+ *<h1>VectorHelper</h1>
+ * Le programme <b>VectorHelper</b> offre divers opÃ©rations sur les vecteurs,
+ * (i.e le tri, la somme de deux vecteurs, l'inversement des Ã©lÃ©ments d'un vecteur, le min et max
+ * et la multiplication des Ã©lÃ©ments par 2).
  * <p>
  * @author Aimene Hichem BOUZARA
  * @author fa_bouzara@esi.dz
@@ -12,6 +12,15 @@
  * @since 15-10-2017
  */
 public class VectorHelper {
+
+    /**
+     *
+     * @param args
+     */
+    public String init(){
+        return "Hello World";
+    }
+    
     public static void main(String[] args) {
         System.out.println("Good morning");
     }
